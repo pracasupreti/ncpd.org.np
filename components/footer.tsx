650c57c58675a78e07e57ca2fe174c0f7e32a9d3
Sanjana Yadav <126410251+SanjanaYdv1@users.noreply.github.com>
2025-07-05 15:40:06 +0545

    updated

--- components/footer.tsx
+++ components/footer.tsx
@@ -90,7 +90,7 @@ export default function Footer() {
       </div>
 
       {/* Footer Section */}
-      <footer className="bg-slate-200 text-gray-700 px-24 py-6">
+      <footer className="bg-slate-200 text-gray-700 lg:px-24 py-6 md:px-16 px-8">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 lg:py-10">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 lg:gap-8">
           {/* About NCPD */}
--- components/ui/map.tsx
+++ components/ui/map.tsx
@@ -5,7 +5,7 @@ const MapSection: React.FC = () => {
   const googleMapsEmbedUrl = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3532.74837583648!2d85.3134633150244!3d27.69408298279893!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb19a1a1a1a1a1%3A0x1a1a1a1a1a1a1a1a!2sAgeno%20Restro!5e0!3m2!1sen!2snp!4v1678901234567!5m2!1sen!2snp";
 
   return (
-    <div className="min-h-screen bg-gray-100 font-sans text-gray-800">
+    <div className="bg-gray-100 font-sans text-gray-800">
 
       {/* Header */}
       {/* <header className="bg-white py-4 shadow-sm">
@@ -16,7 +16,7 @@ const MapSection: React.FC = () => {
         </div>
       </header> */}
 
-      <main className="container mx-auto px-4 md:px-8 lg:px-16 py-8">
+      <main className="container mx-auto px-4 md:px-8 lg:px-16 py-0 lg:py-8">
         <div className="overflow-hidden">
 
           {/* Google Map Embed */}
